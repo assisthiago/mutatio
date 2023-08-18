8d477065ef4fc29da9bba005a49f798033505e64
Thiago Assis <thiago.medassis@gmail.com>
2023-08-18 11:55:17 -0300

    Test without migrations

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app.core",
+    "test_without_migrations",
+    "app.employer",
 ]
 
 MIDDLEWARE = [
