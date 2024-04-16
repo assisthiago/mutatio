dafb38781eb4d82c0d014ca31a060c92ae3d82b0
Thiago Assis <thiago.medassis@gmail.com>
2024-04-16 18:21:25 -0300

    Removing employer app

--- app/employer/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app.employer"
-    verbose_name = "Equipe"
