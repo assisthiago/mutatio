4348c38ec0a83c4d3c2607a8dddb357dc5163427
Thiago Assis <thiago.medassis@gmail.com>
2024-08-13 19:19:20 -0300

    Changing Access and Token lifetime

--- app/settings.py
+++ app/settings.py
@@ -160,7 +160,7 @@ CORS_ALLOWED_ORIGINS = [
 
 # Simple JWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
